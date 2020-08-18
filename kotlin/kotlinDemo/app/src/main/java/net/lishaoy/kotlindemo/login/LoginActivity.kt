a2da9043e58124204421138267e5711e7c4c711a
persilee <i@lishaoy.net>
2020-08-18 18:49:02 +0800

    🥑 add login activity layout

--- kotlin/kotlinDemo/app/src/main/java/net/lishaoy/kotlindemo/login/LoginActivity.kt
+++ kotlin/kotlinDemo/app/src/main/java/net/lishaoy/kotlindemo/login/LoginActivity.kt
@@ -2,11 +2,19 @@ package net.lishaoy.kotlindemo.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gyf.immersionbar.ImmersionBar
 import net.lishaoy.kotlindemo.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        ImmersionBar.with(this)
+            .statusBarColor(R.color.top_bg_color)
+            .fitsSystemWindows(true)
+            .init();
     }
 }
