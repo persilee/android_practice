21018b93afc22980615c87164188e775f82e05bf
persilee <i@lishaoy.net>
2020-09-13 09:26:21 +0800

    🍒 add bottom app bar and nextedscroll

---
+++ UI/MaterialDesignDemo/app/src/main/java/net/lishaoy/materialdesigndemo/adapter/BottomAppBarRecyclerAdapter.java
@@ -0,0 +1,60 @@
+package net.lishaoy.materialdesigndemo.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import net.lishaoy.materialdesigndemo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BottomAppBarRecyclerAdapter extends RecyclerView.Adapter<BottomAppBarRecyclerAdapter.BottomViewHolder> {
+
+    Context context;
+    List<Integer> lists = new ArrayList<>();
+
+    public BottomAppBarRecyclerAdapter(Context context) {
+        this.context = context;
+        this.lists = getLists();
+    }
+
+    @NonNull
+    @Override
+    public BottomAppBarRecyclerAdapter.BottomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new BottomViewHolder(LayoutInflater.from(context).inflate(R.layout.bottom_bar_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BottomAppBarRecyclerAdapter.BottomViewHolder holder, int position) {
+        holder.textView.setText(lists.get(position).toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return lists.size();
+    }
+
+    private List<Integer> getLists() {
+        for (int i = 0; i < 66; i++) {
+            lists.add(i);
+        }
+
+        return lists;
+    }
+
+    public class BottomViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public BottomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.bottom_bar_item_text);
+        }
+    }
+}
--- UI/MaterialDesignDemo/app/src/main/java/net/lishaoy/materialdesigndemo/appbars/BottomAppBarActivity.java
+++ UI/MaterialDesignDemo/app/src/main/java/net/lishaoy/materialdesigndemo/appbars/BottomAppBarActivity.java
@@ -1,11 +1,24 @@
 package net.lishaoy.materialdesigndemo.appbars;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+
 import net.lishaoy.materialdesigndemo.R;
+import net.lishaoy.materialdesigndemo.adapter.BottomAppBarRecyclerAdapter;
 
 public class BottomAppBarActivity extends AppCompatActivity {
+
+    private BottomAppBar bottomAppBar;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,32 @@ public class BottomAppBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_app_bar);
 
         getSupportActionBar().hide();
+
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(BottomAppBarActivity.this, "bottom bar menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.search:
+                        Toast.makeText(BottomAppBarActivity.this, "search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.more:
+                        Toast.makeText(BottomAppBarActivity.this, "more", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+        recyclerView = findViewById(R.id.bottom_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new BottomAppBarRecyclerAdapter(this));
     }
 }
